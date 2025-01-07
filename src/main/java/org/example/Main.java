578703945e16207b08b7ec35e8271833fc941195
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2025-01-07 13:33:48 +0300

    görev 1 & görev 2 completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,29 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static boolean isPalindrome(int number) {
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        while(number != 0) {
+            reversedNumber = reversedNumber * 10 + number % 10;
+            number /= 10;
+        }
+        return originalNumber == reversedNumber;
+    }
+
+    public static boolean isPerfectNumber (int number) {
+        if(number <= 1) {
+            return false;
+        }
+        int sum = 1;
+        for (int i = 2; i < number/2; i++) { //Döngü sadece number / 2'ye kadar yapılır çünkü bir sayının bölenlerinin en büyüğü yarısından fazlası olamaz.
+            if(number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == number;
+    }
+
 }
