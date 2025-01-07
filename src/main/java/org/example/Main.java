eb86f2593b9c124f2f22a52bb66aa194602cdee1
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2025-01-07 14:29:02 +0300

    finish

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -29,4 +29,32 @@ public class Main {
         return sum == number;
     }
 
+    public static String numberToWords(int n) {
+        if (n < 0) {
+            return "Invalid Value";
+        }
+
+        StringBuilder result = new StringBuilder(); // Sonuçları biriktirmek için StringBuilder kullanıyoruz
+        String numberAsString = String.valueOf(n);
+
+        for (int i = 0; i < numberAsString.length(); i++) {
+            char digitalChar = numberAsString.charAt(i);
+            switch (digitalChar) {
+                case '0' -> result.append("Zero ");
+                case '1' -> result.append("One ");
+                case '2' -> result.append("Two ");
+                case '3' -> result.append("Three ");
+                case '4' -> result.append("Four ");
+                case '5' -> result.append("Five ");
+                case '6' -> result.append("Six ");
+                case '7' -> result.append("Seven ");
+                case '8' -> result.append("Eight ");
+                case '9' -> result.append("Nine ");
+            }
+        }
+
+        return result.toString().trim(); // Sonucu string olarak döndür, fazla boşlukları kaldır
+    }
+
+
 }
